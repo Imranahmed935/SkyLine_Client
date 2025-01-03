d903729c9301d1dd4f789f8943eea33ced5c041c
Imranahmed935 <imrantahir9918@gmail.com>
2025-01-03 15:10:23 +0600

    skyLine project setup done

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className='text-6xl text-red-600'>Ops... page not found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
